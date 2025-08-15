de00a4980731bbb9241fa6951a9c3592da9dc7e7
Nadya <nb1985@ukr.net>
2025-08-15 18:44:58 +0300

    feat: implement Count component with increment and decrement

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Counter from "./components/Counter";
+
 const App = () => {
 
   return (
-    <div>Hello redux</div>
+    <Counter />
   )
 }
 
---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../app/features/counter/counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <div className="counter">
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>     
+    </div>
+  );
+};
+
+export default Counter;
